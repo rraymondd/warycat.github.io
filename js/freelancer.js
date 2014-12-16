acbd7809d376bb276900133313e9bf37e7192556
Larry Fantasy <larry.fantasy@gmail.com>
2014-12-15 16:36:50 -0800

    rm click

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,31 +35,3 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-$(function(){
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-    if(isMobile.any()){
-        $('#loginBtn').on('tap',function(){
-            window.open('http://weixin.qq.com/r/O3W6olnE9wSIrR3B9yAp');
-        });
-    }
-});
