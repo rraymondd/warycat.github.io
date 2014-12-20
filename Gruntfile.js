61cab83847dd53fe666de97f36c83180480bc2f7
Larry Fantasy <larry.fantasy@gmail.com>
2014-12-19 16:05:46 -0800

    switch to yo

--- Gruntfile.js
+++ Gruntfile.js
@@ -418,7 +418,7 @@ module.exports = function (grunt) {
         'uglify',
         'copy:dist',
         'modernizr',
-        'rev',
+        // 'rev',
         'usemin',
         'htmlmin'
     ]);
